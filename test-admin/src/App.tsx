7326b0704c69bc0ac184449f18857a33f4892660
Coby Walsh <cobywalsh7@yahoo.com>
2025-02-07 08:39:38 -0700

    added post-show, user-show, post-edit and user-show

--- test-admin/src/App.tsx
+++ test-admin/src/App.tsx
@@ -5,11 +5,14 @@ import {
 import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
 import PostList from "./pages/posts/posts-list";
+import PostShow from "./pages/posts/post-show";
+import PostEdit from "./pages/posts/post-edit";
 import UserList from "./pages/users/users-list";
-
+import UserShow from "./pages/users/users-show";
+import UserEdit from "./pages/users/users-edit";
 export const App = () => (
   <Admin layout={Layout} dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList}/>
-    <Resource name="users" list={UserList}/>
+    <Resource name="posts" list={PostList} show={PostShow} edit={PostEdit}/>
+    <Resource name="users" list={UserList} show={UserShow} edit={UserEdit}/>
   </Admin>
 );
